eb6f91e62f65d70b017f09654418d0eb2a2cf69c
Christophe Demko <chdemko@gmail.com>
2017-11-18 13:39:20 +0100

    Correcting test

--- tests/test_tip.py
+++ tests/test_tip.py
@@ -45,7 +45,5 @@ def test_tip():
 
 
     dest = pandoc_latex_tip.tip(src['t'], src['c'], 'latex', meta)
-    assert isinstance(dest, list)
-    assert len(dest) == 2
-    assert dest[1]['t'] == 'RawInline'
+    assert dest['c'][1][0]['c'][0] == 'tex'
 
