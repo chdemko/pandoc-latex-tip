e7976e732966afd670da36433eadf7604741af67
Christophe Demko <chdemko@gmail.com>
2017-11-23 17:41:10 +0100

    Factorize tests

--- tests/test_tip.py
+++ tests/test_tip.py
@@ -5,51 +5,12 @@ from pandocfilters import Para, Str, Space, Span, Div, Strong, RawInline, RawBlo
 import pandoc_latex_tip
 
 def test_span():
-
-    src = Span(['', ['class1', 'class2'], []], [])
-    meta = {
-        'pandoc-latex-tip': {
-            't': 'MetaList',
-            'c': [
-                {
-                    't': 'MetaMap',
-                    'c': {
-                        'classes': {
-                            't': 'MetaList',
-                            'c': [
-                                {
-                                    't': 'MetaInlines',
-                                    'c': [
-                                        {
-                                            't': 'Str',
-                                            'c': 'class1'
-                                        }
-                                    ]
-                                },
-                                {
-                                    't': 'MetaInlines',
-                                    'c': [
-                                        {
-                                            't': 'Str',
-                                            'c': 'class2'
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                    }
-                }
-            ]
-        }
-    }
-
-
-    dest = pandoc_latex_tip.tip(src['t'], src['c'], 'latex', meta)
-    assert dest[0]['c'][0] == 'tex'
+    do_test(Span(['', ['class1', 'class2'], []], []))
 
 def test_div():
+    do_test(Div(['', ['class1', 'class2'], []], []))
 
-    src = Div(['', ['class1', 'class2'], []], [])
+def do_test(src):
     meta = {
         'pandoc-latex-tip': {
             't': 'MetaList',
