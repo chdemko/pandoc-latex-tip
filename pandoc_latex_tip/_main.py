8f05cdd98d7a562ee440ba7ce0a0d0b54bf4405a
Christophe Demko <chdemko@gmail.com>
2024-08-07 16:51:44 +0200

    Preparing 3.0.0.0

--- pandoc_latex_tip/_main.py
+++ pandoc_latex_tip/_main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# pylint: disable=too-many-lines
+
 """
 Pandoc filter for adding tip in LaTeX.
 """
@@ -9,6 +11,7 @@ import operator
 import os
 import pathlib
 import re
+import shutil
 import sys
 import tempfile
 from typing import Any
@@ -20,6 +23,7 @@ import PIL.ImageFont
 
 from cleo.application import Application
 from cleo.commands.command import Command
+from cleo.helpers import argument, option
 
 import fontTools.ttLib
 
@@ -265,6 +269,37 @@ class IconFont:
         out_image.save(os.path.join(export_dir, filename))
 
 
+def get_core_icons() -> list[dict[str, str]]:
+    """
+    Get the core icons.
+
+    Returns
+    -------
+    list[dict[str, str]]
+        The core icons.
+    """
+    return [
+        {
+            "collection": "fontawesome",
+            "css": "fontawesome.css",
+            "ttf": "fa-solid-900.ttf",
+            "prefix": "fa-",
+        },
+        {
+            "collection": "fontawesome",
+            "css": "fontawesome.css",
+            "ttf": "fa-regular-400.ttf",
+            "prefix": "far-",
+        },
+        {
+            "collection": "fontawesome",
+            "css": "brands.css",
+            "ttf": "fa-brands-400.ttf",
+            "prefix": "fab-",
+        },
+    ]
+
+
 def load_icons() -> dict[str, IconFont]:
     """
     Get the icons.
@@ -275,48 +310,43 @@ def load_icons() -> dict[str, IconFont]:
         A dictionnary from icon name to IconFont.
     """
     icons = {}
-    icon_font = IconFont(
-        css_file=pathlib.Path(
-            sys.prefix, "share", "pandoc_latex_tip", "fontawesome", "fontawesome.css"
-        ),
-        ttf_file=pathlib.Path(
-            sys.prefix, "share", "pandoc_latex_tip", "fontawesome", "fa-solid-900.ttf"
-        ),
-    )
-    icons.update({key: icon_font for key in icon_font.css_icons})
-    icon_font = IconFont(
-        css_file=pathlib.Path(
-            sys.prefix, "share", "pandoc_latex_tip", "fontawesome", "fontawesome.css"
-        ),
-        ttf_file=pathlib.Path(
-            sys.prefix, "share", "pandoc_latex_tip", "fontawesome", "fa-regular-400.ttf"
-        ),
-        prefix="far-",
-    )
-    icons.update({key: icon_font for key in icon_font.css_icons})
-    icon_font = IconFont(
-        css_file=pathlib.Path(
-            sys.prefix, "share", "pandoc_latex_tip", "fontawesome", "brands.css"
-        ),
-        ttf_file=pathlib.Path(
-            sys.prefix, "share", "pandoc_latex_tip", "fontawesome", "fa-brands-400.ttf"
-        ),
-    )
-    icons.update({key: icon_font for key in icon_font.css_icons})
+    for definition in get_core_icons():
+        icon_font = IconFont(
+            css_file=pathlib.Path(
+                sys.prefix,
+                "share",
+                "pandoc_latex_tip",
+                definition["collection"],
+                definition["css"],
+            ),
+            ttf_file=pathlib.Path(
+                sys.prefix,
+                "share",
+                "pandoc_latex_tip",
+                definition["collection"],
+                definition["ttf"],
+            ),
+            prefix=definition["prefix"],
+        )
+        icons.update({key: icon_font for key in icon_font.css_icons})
 
     config_path = pathlib.Path(sys.prefix, "share", "pandoc_latex_tip", "config.yml")
     if config_path.exists():
         with config_path.open(encoding="utf-8") as stream:
             config = yaml.safe_load(stream)
-            for collection, definition in config.items():
-                if "css" not in definition:
+            for definition in config:
+                if "collection" not in definition:
                     break
+                collection = definition["collection"]
                 if "css" not in definition:
                     break
                 css_file = definition["css"]
                 if "ttf" not in definition:
                     break
                 ttf_file = definition["ttf"]
+                if "prefix" not in definition:
+                    break
+                prefix = definition["prefix"]
                 icon_font = IconFont(
                     css_file=pathlib.Path(
                         sys.prefix,
@@ -332,7 +362,7 @@ def load_icons() -> dict[str, IconFont]:
                         collection,
                         ttf_file,
                     ),
-                    prefix=definition.get("prefix", False),
+                    prefix=prefix,
                 )
                 icons.update({key: icon_font for key in icon_font.css_icons})
 
@@ -906,24 +936,358 @@ def main(doc: Doc | None = None) -> Doc:
     return run_filter(tip, prepare=prepare, finalize=finalize, doc=doc)
 
 
-class NewCommand(Command):
+class CollectionsAddCommand(Command):
     """
-    NewCommand.
+    CollectionsAddCommand.
     """
 
-    name = "{format}"
-    description = "Add new collection"
+    name = "collections add"
+    description = "Add a file to a collection"
+    arguments = [
+        argument("name", description="Collection name"),
+        argument("file", description="File name"),
+    ]
 
     def handle(self) -> int:
         """
-        Handle new command.
+        Handle collections add command.
 
         Returns
         -------
         int
             status code
         """
-        self.line("New")
+        if self.argument("name") == "fontawesome":
+            self.line("<error>You cannot modify core collection</error>")
+            return 1
+
+        try:
+            dir_path = pathlib.Path(
+                sys.prefix, "share", "pandoc_latex_tip", self.argument("name")
+            )
+            if not dir_path.exists():
+                dir_path.mkdir(parents=True)
+            file_path = pathlib.Path(self.argument("file"))
+            dest_path = pathlib.Path(dir_path, file_path.parts[-1])
+            shutil.copy(file_path, dest_path)
+
+            self.line(
+                f"Add file '{self.argument('file')}' to "
+                f"collection '{self.argument('name')}'"
+            )
+
+        except PermissionError as error:
+            self.line(f"<error>{error}</error>")
+            return 1
+        return 0
+
+
+class CollectionsDeleteCommand(Command):
+    """
+    CollectionDeleteCommand.
+    """
+
+    name = "collections delete"
+    description = "Delete a collection"
+    arguments = [
+        argument("name", description="Collection name"),
+    ]
+
+    def handle(self) -> int:
+        """
+        Handle collections delete command.
+
+        Returns
+        -------
+        int
+            status code
+        """
+        if self.argument("name") == "fontawesome":
+            self.line("<error>You cannot modify core collection</error>")
+            return 1
+        try:
+            dir_path = pathlib.Path(
+                sys.prefix, "share", "pandoc_latex_tip", self.argument("name")
+            )
+            config_path = pathlib.Path(
+                sys.prefix,
+                "share",
+                "pandoc_latex_tip",
+                "config.yml",
+            )
+            try:
+                if config_path.exists():
+                    with config_path.open(encoding="utf-8") as stream:
+                        icons = yaml.safe_load(stream)
+                        for definition in icons:
+                            if definition["collection"] == self.argument("name"):
+                                self.line(
+                                    f"<error>Collection '{self.argument('name')}' "
+                                    f"is in use</error>"
+                                )
+                                return 1
+            except PermissionError as error:
+                self.line(f"<error>{error}</error>")
+                return 1
+
+            if dir_path.exists():
+                shutil.rmtree(dir_path)
+                self.line(f"Delete collection '{self.argument('name')}'")
+            else:
+                self.line(
+                    f"<error>Collection '{self.argument('name')}' "
+                    f"does not exist</error>"
+                )
+                return 1
+        except PermissionError as error:
+            self.line(f"<error>{error}</error>")
+            return 1
+        return 0
+
+
+class CollectionsListCommand(Command):
+    """
+    CollectionsListCommand.
+    """
+
+    name = "collections"
+    description = "List the collections"
+
+    def handle(self) -> int:
+        """
+        Handle collections command.
+
+        Returns
+        -------
+        int
+            status code
+        """
+        dir_path = pathlib.Path(sys.prefix, "share", "pandoc_latex_tip")
+        for folder in dir_path.iterdir():
+            if folder.parts[-1] == "fontawesome":
+                self.line("<info>fontawesome *</info>")
+            elif folder.is_dir():
+                self.line(folder.parts[-1])
+        return 0
+
+
+class CollectionsInfoCommand(Command):
+    """
+    CollectionsInfoCommand.
+    """
+
+    name = "collections info"
+    description = "Display a collection"
+    arguments = [
+        argument("name", description="Collection name"),
+    ]
+
+    def handle(self) -> int:
+        """
+        Handle collections info command.
+
+        Returns
+        -------
+        int
+            status code
+        """
+        dir_path = pathlib.Path(
+            sys.prefix,
+            "share",
+            "pandoc_latex_tip",
+            self.argument("name"),
+        )
+        if dir_path.exists():
+            for filename in dir_path.iterdir():
+                self.line(filename.parts[-1])
+        else:
+            self.line(
+                f"<error>Collection '{self.argument('name')}' "
+                f"does not exist</error>"
+            )
+            return 1
+        return 0
+
+
+class IconsAddCommand(Command):
+    """
+    IconsAddCommand.
+    """
+
+    name = "icons add"
+    description = "Add a set of icons from a collection"
+    arguments = [
+        argument("name", description="Collection name"),
+    ]
+    options = [
+        option("css", flag=False, description="css filename from the collection"),
+        option("ttf", flag=False, description="ttf filename from the collection"),
+        option("prefix", flag=False, description="icon prefix"),
+    ]
+
+    def handle(self) -> int:
+        """
+        Handle icons add command.
+
+        Returns
+        -------
+        int
+            status code
+        """
+        if self.argument("name") == "fontawesome":
+            self.line("<error>You cannot modify core collection</error>")
+            return 1
+        dir_path = pathlib.Path(
+            sys.prefix,
+            "share",
+            "pandoc_latex_tip",
+            self.argument("name"),
+        )
+        if dir_path.exists():
+            css_file = pathlib.Path(dir_path, self.option("css"))
+            if not css_file.exists():
+                self.line(
+                    f"<error>Collection '{self.argument('name')}' "
+                    f"does not contain '{self.option('css')}'</error>"
+                )
+                return 1
+            ttf_file = pathlib.Path(dir_path, self.option("ttf"))
+            if not ttf_file.exists():
+                self.line(
+                    f"<error>Collection '{self.argument('name')}' "
+                    f"does not contain '{self.option('ttf')}'</error>"
+                )
+                return 1
+            config_path = pathlib.Path(
+                sys.prefix,
+                "share",
+                "pandoc_latex_tip",
+                "config.yml",
+            )
+            try:
+                if config_path.exists():
+                    with config_path.open(encoding="utf-8") as stream:
+                        icons = yaml.safe_load(stream)
+                        for definition in icons:
+                            if definition["prefix"] == self.option("prefix"):
+                                self.line(
+                                    f"<error>Prefix '{self.option('prefix')}' "
+                                    f"is already used</error>"
+                                )
+                                return 1
+                else:
+                    icons = []
+                icons.append(
+                    {
+                        "collection": self.argument("name"),
+                        "css": self.option("css"),
+                        "ttf": self.option("ttf"),
+                        "prefix": self.option("prefix"),
+                    },
+                )
+                with config_path.open(mode="w", encoding="utf-8") as stream:
+                    stream.write(yaml.dump(icons, sort_keys=False))
+            except PermissionError as error:
+                self.line(f"<error>{error}</error>")
+                return 1
+
+        else:
+            self.line(
+                f"<error>Collection '{self.argument('name')}' "
+                f"does not exist</error>"
+            )
+            return 1
+        return 0
+
+
+class IconsDeleteCommand(Command):
+    """
+    IconsDeleteCommand.
+    """
+
+    name = "icons delete"
+    description = "Delete a set of icons"
+    options = [
+        option("prefix", flag=False, description="icon prefix"),
+    ]
+
+    def handle(self) -> int:
+        """
+        Handle icons delete command.
+
+        Returns
+        -------
+        int
+            status code
+        """
+        if self.option("prefix") in ("fa-", "far-", "fab-"):
+            self.line("<error>You cannot modify core icons</error>")
+            return 1
+        config_path = pathlib.Path(
+            sys.prefix,
+            "share",
+            "pandoc_latex_tip",
+            "config.yml",
+        )
+        try:
+            if config_path.exists():
+                with config_path.open(encoding="utf-8") as stream:
+                    icons = yaml.safe_load(stream)
+                keep = [
+                    definition
+                    for definition in icons
+                    if definition["prefix"] != self.option("prefix")
+                ]
+                if keep != icons:
+                    with config_path.open(mode="w", encoding="utf-8") as stream:
+                        stream.write(yaml.dump(keep, sort_keys=False))
+                else:
+                    self.line("<error>Unexisting prefix</error>")
+                    return 1
+            else:
+                self.line("<error>Unexisting config file</error>")
+                return 1
+
+        except PermissionError as error:
+            self.line(f"<error>{error}</error>")
+            return 1
+        return 0
+
+
+class IconsListCommand(Command):
+    """
+    IconsListCommand.
+    """
+
+    name = "icons"
+    description = "List the set of icons"
+
+    def handle(self) -> int:
+        """
+        Handle icons command.
+
+        Returns
+        -------
+        int
+            status code
+        """
+        icons = get_core_icons()
+        config_path = pathlib.Path(
+            sys.prefix,
+            "share",
+            "pandoc_latex_tip",
+            "config.yml",
+        )
+        try:
+            if config_path.exists():
+                with config_path.open(encoding="utf-8") as stream:
+                    icons.extend(yaml.safe_load(stream))
+        except PermissionError as error:
+            self.line(f"<error>{error}</error>")
+            return 1
+
+        self.line(yaml.dump(icons, sort_keys=False))
+
         return 0
 
 
@@ -974,7 +1338,13 @@ def app() -> None:
     Create a cleo application.
     """
     application = Application("Pandoc LaTeX Tip Filter")
-    application.add(NewCommand())
+    application.add(CollectionsAddCommand())
+    application.add(CollectionsDeleteCommand())
+    application.add(CollectionsListCommand())
+    application.add(CollectionsInfoCommand())
+    application.add(IconsAddCommand())
+    application.add(IconsDeleteCommand())
+    application.add(IconsListCommand())
     application.add(PandocLaTeXFilterCommand())
     application.add(PandocBeamerFilterCommand())
     application.run()
