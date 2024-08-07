76289b720ba6859e545e124c48bc2e7f6baa85c2
Christophe Demko <chdemko@gmail.com>
2024-08-07 12:27:36 +0200

    Prepare version 3.0.0.0

---
+++ pandoc_latex_tip/__init__.py
@@ -0,0 +1,10 @@
+"""
+pandoc_latex_tip package.
+"""
+
+from ._main import app, main
+
+__all__ = ("main", "app")
+
+if __name__ == "__main__":
+    main()
--- tests/test_tip.py
+++ tests/test_tip.py
@@ -1,153 +1,451 @@
-# This Python file uses the following encoding: utf-8
-
 from unittest import TestCase
 
-from panflute import (
-    MetaList,
-    MetaMap,
-    MetaString,
-    Doc,
-    Para,
-    Span,
-    Div,
-    Code,
-    CodeBlock,
-)
+from panflute import convert_text
 
 import pandoc_latex_tip
 
 
 class TipTest(TestCase):
-    @classmethod
-    def metadata(cls):
-        return {
-            "pandoc-latex-tip": MetaList(
-                MetaMap(
-                    classes=MetaList(MetaString("tip"), MetaString("listing")),
-                    icons=MetaList(
-                        MetaMap(
-                            name=MetaString("file-text"),
-                            color=MetaString("darksalmon"),
-                            link=MetaString("http://www.google.fr"),
-                        ),
-                        MetaString("comments"),
-                    ),
-                    size=MetaString("36"),
-                    position=MetaString("right"),
-                ),
-                MetaMap(
-                    classes=MetaList(MetaString("warning")),
-                    icons=MetaString("comments"),
-                ),
-                MetaMap(
-                    classes=MetaList(MetaString("tip")), position=MetaString("left")
-                ),
-                MetaMap(
-                    classes=MetaList(MetaString("v5.0")),
-                    icons=MetaList(
-                        MetaMap(
-                            name=MetaString("balance-scale"),
-                            version=MetaString("5.x"),
-                            variant=MetaString("solid"),
-                            color=MetaString("orange"),
-                        )
-                    ),
-                ),
-            )
-        }
+    def verify_conversion(
+        self,
+        text,
+        expected,
+        transform,
+        input_format="markdown",
+        output_format="latex",
+        standalone=False,
+    ) -> None:
+        """
+        Verify the conversion.
+
+        Parameters
+        ----------
+        text
+            input text
+        expected
+            expected text
+        transform
+            filter function
+        input_format
+            input format
+        output_format
+            output format
+        standalone
+            is the output format standalone ?
+        """
+        doc = convert_text(text, input_format=input_format, standalone=True)
+        doc.format = output_format
+        doc = transform(doc)
+        converted = convert_text(
+            doc.content,
+            input_format="panflute",
+            output_format=output_format,
+            extra_args=["--wrap=none"],
+            standalone=standalone,
+        )
+        self.assertEqual(converted.strip(), expected.strip())
 
     def test_span(self):
-        doc = Doc(
-            Para(
-                Span(classes=["tip", "listing"]),
-                Span(classes=["tip"]),
-                Span(classes=["warning"]),
-                Span(classes=["v5.0"]),
-                Span(
-                    attributes={
-                        "latex-tip-icon": "warning",
-                        "latex-tip-position": "right",
-                        "latex-tip-size": 24,
-                    }
-                ),
-            ),
-            metadata=self.metadata(),
-            format="latex",
-        )
-        pandoc_latex_tip.main(doc)
-        self.assertEqual(doc.content[0].content[1].format, "tex")
-        self.assertEqual(doc.content[0].content[3].format, "tex")
-        self.assertEqual(doc.content[0].content[5].format, "tex")
-        self.assertEqual(doc.content[0].content[7].format, "tex")
-        self.assertEqual(doc.content[0].content[9].format, "tex")
+        self.verify_conversion(
+            """
+---
+pandoc-latex-tip:
+  - classes: [tip, listing]
+    icons:
+      - {name: fa-file-text, color: darksalmon, link: http://www.google.fr}
+      - fa-comments
+    size: 36
+    position: right
 
-    def test_div(self):
-        doc = Doc(
-            Div(classes=["tip", "listing"]),
-            Div(classes=["tip"]),
-            Div(classes=["warning"]),
-            Div(
-                attributes={
-                    "latex-tip-icon": "warning",
-                    "latex-tip-position": "right",
-                    "latex-tip-size": 24,
-                }
-            ),
-            metadata=self.metadata(),
-            format="latex",
-        )
-        pandoc_latex_tip.main(doc)
-        self.assertEqual(doc.content[0].format, "tex")
-        self.assertEqual(doc.content[1].format, "tex")
-        self.assertEqual(doc.content[3].format, "tex")
-        self.assertEqual(doc.content[4].format, "tex")
-        self.assertEqual(doc.content[6].format, "tex")
-        self.assertEqual(doc.content[7].format, "tex")
-        self.assertEqual(doc.content[9].format, "tex")
-        self.assertEqual(doc.content[10].format, "tex")
+  - classes: [warning]
+    icons: fa-comments
 
-    def test_code(self):
-        doc = Doc(
-            Para(
-                Code("", classes=["tip", "listing"]),
-                Code("", classes=["tip"]),
-                Code("", classes=["warning"]),
-                Code(
-                    "",
-                    attributes={
-                        "latex-tip-icon": "warning",
-                        "latex-tip-position": "right",
-                        "latex-tip-size": 24,
-                    },
-                ),
-            ),
-            metadata=self.metadata(),
-            format="latex",
+  - classes: [tip]
+    position: left
+
+  - classes: [v5.0]
+    icons:
+      - name: far-user
+        color: orange
+---
+
+[]{.tip .listing}[]{.tip}[]{.warning}[]{.v5.0}
+            """,
+            """
+{}
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddright%%
+\\else%%
+\\pandoclatextipevenright%%
+\\fi%%
+\\marginnote{\\href{http://www.google.fr}{\\includegraphics[width=0.5in,height=0.5in]{/home/chdemko/.cache/pandoc_latex_tip/darksalmon/fa-file-text.png}}\\includegraphics[width=0.5in,height=0.5in]{/home/chdemko/.cache/pandoc_latex_tip/black/fa-comments.png}}[0pt]\\vspace{0cm}%%
+{}
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddleft%%
+\\else%%
+\\pandoclatextipevenleft%%
+\\fi%%
+\\marginnote{\\includegraphics[width=0.25in,height=0.25in]{/home/chdemko/.cache/pandoc_latex_tip/black/fa-exclamation-circle.png}}[0pt]\\vspace{0cm}%%
+{}
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddleft%%
+\\else%%
+\\pandoclatextipevenleft%%
+\\fi%%
+\\marginnote{\\includegraphics[width=0.25in,height=0.25in]{/home/chdemko/.cache/pandoc_latex_tip/black/fa-comments.png}}[0pt]\\vspace{0cm}%%
+{}
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddleft%%
+\\else%%
+\\pandoclatextipevenleft%%
+\\fi%%
+\\marginnote{\\includegraphics[width=0.25in,height=0.25in]{/home/chdemko/.cache/pandoc_latex_tip/orange/far-user.png}}[0pt]\\vspace{0cm}%%
+            """,
+            pandoc_latex_tip.main,
         )
-        pandoc_latex_tip.main(doc)
-        self.assertEqual(doc.content[0].content[1].format, "tex")
-        self.assertEqual(doc.content[0].content[3].format, "tex")
-        self.assertEqual(doc.content[0].content[5].format, "tex")
-        self.assertEqual(doc.content[0].content[7].format, "tex")
 
     def test_codeblock(self):
-        doc = Doc(
-            CodeBlock("", classes=["tip", "listing"]),
-            CodeBlock("", classes=["tip"]),
-            CodeBlock("", classes=["warning"]),
-            CodeBlock(
-                "",
-                attributes={
-                    "latex-tip-icon": "warning",
-                    "latex-tip-position": "right",
-                    "latex-tip-size": 24,
-                },
-            ),
-            metadata=self.metadata(),
-            format="latex",
+        self.verify_conversion(
+            """
+---
+pandoc-latex-tip:
+  - classes: [tip, listing]
+    icons:
+      - {name: fa-file-text, color: darksalmon, link: http://www.google.fr}
+      - fa-comments
+    size: 36
+    position: right
+
+  - classes: [warning]
+    icons: fa-comments
+
+  - classes: [tip]
+    position: left
+
+  - classes: [v5.0]
+    icons:
+      - name: far-user
+        color: orange
+---
+
+~~~{.python .warning}
+main()
+~~~
+
+            """,
+            """
+\\begin{minipage}{\\textwidth}
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddleft%%
+\\else%%
+\\pandoclatextipevenleft%%
+\\fi%%
+\\marginnote{\\includegraphics[width=0.25in,height=0.25in]{/home/chdemko/.cache/pandoc_latex_tip/black/fa-comments.png}}[0pt]\\vspace{0cm}%%
+
+\\begin{Shaded}
+\\begin{Highlighting}[]
+\\NormalTok{main()}
+\\end{Highlighting}
+\\end{Shaded}
+
+\\end{minipage}
+
+            """,
+            pandoc_latex_tip.main,
         )
-        pandoc_latex_tip.main(doc)
-        self.assertEqual(doc.content[0].format, "tex")
-        self.assertEqual(doc.content[2].format, "tex")
-        self.assertEqual(doc.content[5].format, "tex")
-        self.assertEqual(doc.content[8].format, "tex")
+
+    def test_div(self):
+        self.verify_conversion(
+            """
+---
+pandoc-latex-tip:
+  - classes: [tip, listing]
+    icons:
+      - {name: fa-file-text, color: darksalmon, link: http://www.google.fr}
+      - fa-comments
+    size: 36
+    position: right
+
+  - classes: [warning]
+    icons: fa-comments
+
+  - classes: [tip]
+    position: left
+
+  - classes: [v5.0]
+    icons:
+      - name: far-user
+        color: orange
+---
+
+::: warning
+Division
+:::
+            """,
+            """
+Division
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddleft%%
+\\else%%
+\\pandoclatextipevenleft%%
+\\fi%%
+\\marginnote{\\includegraphics[width=0.25in,height=0.25in]{/home/chdemko/.cache/pandoc_latex_tip/black/fa-comments.png}}[0pt]\\vspace{0cm}%%
+            """,
+            pandoc_latex_tip.main,
+        )
+        self.verify_conversion(
+            """
+::: {
+  latex-tip-icon=fa-address-book
+  latex-tip-size=24
+  latex-tip-position=right
+  latex-tip-color=lightskyblue} :::
+Division
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+            """,
+            """
+Division
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddright%%
+\\else%%
+\\pandoclatextipevenright%%
+\\fi%%
+\\marginnote{\\includegraphics[width=0.33333in,height=0.33333in]{/home/chdemko/.cache/pandoc_latex_tip/lightskyblue/fa-address-book.png}}[0pt]\\vspace{0cm}%%
+            """,
+            pandoc_latex_tip.main,
+        )
+
+    def test_div_div(self):
+        self.verify_conversion(
+            """
+---
+pandoc-latex-tip:
+  - classes: [tip, listing]
+    icons:
+      - {name: fa-file-text, color: darksalmon, link: http://www.google.fr}
+      - fa-comments
+    size: 36
+    position: right
+
+  - classes: [warning]
+    icons: fa-comments
+
+  - classes: [tip]
+    position: left
+
+  - classes: [v5.0]
+    icons:
+      - name: far-user
+        color: orange
+---
+
+::: warning
+::: div
+Division
+:::
+:::
+            """,
+            """
+Division
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddleft%%
+\\else%%
+\\pandoclatextipevenleft%%
+\\fi%%
+\\marginnote{\\includegraphics[width=0.25in,height=0.25in]{/home/chdemko/.cache/pandoc_latex_tip/black/fa-comments.png}}[0pt]\\vspace{0cm}%%
+            """,
+            pandoc_latex_tip.main,
+        )
+
+    def test_div_rule(self):
+        self.verify_conversion(
+            """
+---
+pandoc-latex-tip:
+  - classes: [tip, listing]
+    icons:
+      - {name: fa-file-text, color: darksalmon, link: http://www.google.fr}
+      - fa-comments
+    size: 36
+    position: right
+
+  - classes: [warning]
+    icons: fa-comments
+
+  - classes: [tip]
+    position: left
+
+  - classes: [v5.0]
+    icons:
+      - name: far-user
+        color: orange
+---
+
+::: warning
+-----------
+:::
+            """,
+            """
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddleft%%
+\\else%%
+\\pandoclatextipevenleft%%
+\\fi%%
+\\marginnote{\\includegraphics[width=0.25in,height=0.25in]{/home/chdemko/.cache/pandoc_latex_tip/black/fa-comments.png}}[0pt]\\vspace{0cm}%%
+
+\\begin{center}\\rule{0.5\\linewidth}{0.5pt}\\end{center}
+            """,
+            pandoc_latex_tip.main,
+        )
+
+    def test_div_lineblock(self):
+        self.verify_conversion(
+            """
+---
+pandoc-latex-tip:
+  - classes: [tip, listing]
+    icons:
+      - {name: fa-file-text, color: darksalmon, link: http://www.google.fr}
+      - fa-comments
+    size: 36
+    position: right
+
+  - classes: [warning]
+    icons: fa-comments
+
+  - classes: [tip]
+    position: left
+
+  - classes: [v5.0]
+    icons:
+      - name: far-user
+        color: orange
+---
+
+::: warning
+| Lineblock
+| continue
+:::
+            """,
+            """
+Lineblock
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddleft%%
+\\else%%
+\\pandoclatextipevenleft%%
+\\fi%%
+\\marginnote{\\includegraphics[width=0.25in,height=0.25in]{/home/chdemko/.cache/pandoc_latex_tip/black/fa-comments.png}}[0pt]\\vspace{0cm}%%
+\\\\
+continue
+            """,
+            pandoc_latex_tip.main,
+        )
+
+    def test_div_codeblock(self):
+        self.verify_conversion(
+            """
+---
+pandoc-latex-tip:
+  - classes: [tip, listing]
+    icons:
+      - {name: fa-file-text, color: darksalmon, link: http://www.google.fr}
+      - fa-comments
+    size: 36
+    position: right
+
+  - classes: [warning]
+    icons: fa-comments
+
+  - classes: [tip]
+    position: left
+
+  - classes: [v5.0]
+    icons:
+      - name: far-user
+        color: orange
+---
+
+::: warning
+~~~python
+main()
+~~~
+:::
+            """,
+            """
+\\begin{minipage}{\\textwidth}
+\\checkoddpage%%
+\\ifoddpage%%
+\\pandoclatextipoddleft%%
+\\else%%
+\\pandoclatextipevenleft%%
+\\fi%%
+\\marginnote{\\includegraphics[width=0.25in,height=0.25in]{/home/chdemko/.cache/pandoc_latex_tip/black/fa-comments.png}}[0pt]\\vspace{0cm}%%
+
+\\begin{Shaded}
+\\begin{Highlighting}[]
+\\NormalTok{main()}
+\\end{Highlighting}
+\\end{Shaded}
+
+\\end{minipage}
+            """,
+            pandoc_latex_tip.main,
+        )
+
+    def test_div_bulletlist(self):
+        self.verify_conversion(
+            """
+---
+pandoc-latex-tip:
+  - classes: [tip, listing]
+    icons:
+      - {name: fa-file-text, color: darksalmon, link: http://www.google.fr}
+      - fa-comments
+    size: 36
+    position: right
+
+  - classes: [warning]
+    icons: fa-comments
+
+  - classes: [tip]
+    position: left
+
+  - classes: [v5.0]
+    icons:
+      - name: far-user
+        color: orange
+---
+
+::: warning
+* a
+* b
+:::
+            """,
+            """
+\\begin{itemize}
+\\tightlist
+\\item
+  a
+  \\checkoddpage%%
+  \\ifoddpage%%
+  \\pandoclatextipoddleft%%
+  \\else%%
+  \\pandoclatextipevenleft%%
+  \\fi%%
+  \\marginnote{\\includegraphics[width=0.25in,height=0.25in]{/home/chdemko/.cache/pandoc_latex_tip/black/fa-comments.png}}[0pt]\\vspace{0cm}%%
+\\item
+  b
+\\end{itemize}
+            """,
+            pandoc_latex_tip.main,
+        )
